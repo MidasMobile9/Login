95762d2e625ee42d1614397b9869570ac66b816f
tyrano <tyranosaurus@nate.com>
2018-05-25 02:25:53 +0900

    create NetworkDefine - PIJ, OSH, CYJ

---
+++ app/src/main/java/com/test/login/network/NetworkDefineCYJ.java
@@ -0,0 +1,11 @@
+package com.test.login.network;
+
+public class NetworkDefineCYJ {
+    // Host 주소
+    public static final String HOST_URL = "http://35.187.156.145:3000";
+
+    //요청 URL path
+    static {
+
+    }
+}
