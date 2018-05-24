a4a525cc6a5b4838027fdedbd3c020519721109e
tyrano <tyranosaurus@nate.com>
2018-05-24 21:07:03 +0900

    add NETWORK package

---
+++ app/src/main/java/com/test/login/network/NetworkDefineConstant.java
@@ -0,0 +1,13 @@
+package com.test.login.network;
+
+public class NetworkDefineConstant {
+    // Host 주소
+    public static final String HOST_URL = "http://35.187.156.145:3000";
+
+    //요청 URL path
+    // ex) public static String SERVER_URL_NAME;
+
+    static {
+        // ex) SERVER_URL_NAME = HOST_URL + "[/aaa/bbb/param1]";
+    }
+}
